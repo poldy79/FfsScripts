6f83b4b9e8f518212ce0455b4bd253a3c15bb50a
Leonard <leonard@penzer.de>
2016-05-24 23:26:56 +0200

    cleanup

--- update_peers.py
+++ update_peers.py
@@ -5,32 +5,47 @@ import psutil
 import signal
 import git
 import argparse
-
+import json
+import socket
+import time
 def test():
     before = getAllKeys("/etc/fastd/peers-ffs")
     after = getAllKeys("/etc/fastd/peers-ffs")
     after["vpn04"].pop()
-    shrinkedSegments = getShrinkedSegments(before, after)
+    #shrinkedSegments = getShrinkedSegments(before, after)
     segmentPids = getProcesses()
+    sockets = getSockets(segmentPids)
+    activeKeys = getAllActiveKeys(sockets)
+    shrinkedSegments = getShrinkedSegments(activeKeys,after)
     for segment in segmentPids:
         pid = segmentPids[segment] 
-        print "Sending SIGHUP to %i"%(pid)
+        print "Would send SIGHUP to %i"%(pid)
         #os.kill(pid,signal.SIGHUP)
     for segment in shrinkedSegments:
         pid = segmentPids[segment]
-        print "Will send SIGUSR2 to %i"%(pid)
+        print "Would send SIGUSR2 to %i"%(pid)
         #os.kill(pid,signal.SIGUSR2)
 
+def readFromSocket(f):
+    s = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
+    s.connect(f)
+    result = ""
+    while(True):
+        tmp = s.recv(1024*1024)
+        if tmp == "":
+            break
+        result += tmp
+    s.close()
+    return result
 
-def _testRemoveKey(segmentkeys):
-    import copy
-    import random
-    segmentkeys = copy.deepcopy(segmentkeys)
-    k = segmentkeys.keys()
-    segment = k[random.randint(0,len(k)-1)]
-    removedKey = segmentkeys[segment].pop(random.randint(0,len(segmentkeys[segment])-1))
-    print "Removed %s from %s"%(removedKey,segment)
-    return segmentkeys
+def getActiveKeys(f):
+    data = readFromSocket(f)
+    status = json.loads(data)
+    keys = []
+    for p in status["peers"]:
+        if  status["peers"][p]["connection"] != None:
+            keys.append(p)
+    return keys
 
 def getProcesses():
     pids = {}
@@ -41,10 +56,25 @@ def getProcesses():
             pass
         else:
             if pinfo["name"] == "fastd":
+                socket = ""
                 config = pinfo["cmdline"][pinfo["cmdline"].index("--config")+1]
                 segment = os.path.dirname(config).rsplit("/",1)[1]
                 pids[segment] = pinfo["pid"]
+                p = psutil.Process(pinfo["pid"])
+                for f in p.get_connections(kind="unix")[0].laddr:
+                    if "sock" in f:
+                        socket = f
     return pids
+
+def getSockets(pids):
+    sockets = {}
+    for segment in pids:
+        pid = pids[segment]
+        p = psutil.Process(pid)
+        for f in p.get_connections(kind="unix"):
+            if "sock" in f.laddr:
+                sockets[segment] = f.laddr
+    return sockets
 
 def getSegments(basedir):
     segments = []
@@ -78,6 +108,12 @@ def printSegmentkeys(segmentkeys):
         for k in keys:
             print "\t%s"%(k)
 
+def getAllActiveKeys(sockets):
+    activeKeys = {}
+    for s in sockets:
+        activeKeys[s] = getActiveKeys(sockets[s])
+    return activeKeys
+
 def getAllKeys(basedir):
     segments =  getSegments(basedir)
     segmentkeys = {}
@@ -88,7 +124,7 @@ def getAllKeys(basedir):
 
 def getShrinkedSegments(before,after):
     shrinkedSegments = {}
-    for s in before:
+    for s in after:
         for key in before[s]:
             if not key in after[s]:
                 shrinkedSegments[s] = True
@@ -97,8 +133,13 @@ def getShrinkedSegments(before,after):
 def gitPull(basedir):
     repo = git.Repo(basedir)
     o = repo.remotes.origin
-    o.pull()
-
+    try:
+        time.sleep(1)
+        o.pull()
+    except:
+        print "Error during pull!"
+        return False
+    return True
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Updates peer files and sends signals to fastd')
@@ -106,19 +147,23 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-
     basedir = args.basedir
-    segmentkeys_before = getAllKeys(basedir)
 
-    gitPull(basedir)
+    result = False
+    retries = 3
+    while not result:
+        result = gitPull(basedir)
+        retries -=1
+        if retries == 0:
+            break
 
     segmentkeys_after = getAllKeys(basedir)
-    #segmentkeys_after = _testRemoveKey(segmentkeys_after)
 
-    #printSegmentkeys(segmentkeys_before)
-    #printSegmentkeys(segmentkeys_after)
-    shrinkedSegments = getShrinkedSegments(segmentkeys_before, segmentkeys_after)
     segmentPids = getProcesses()
+    sockets = getSockets(segmentPids)
+    activeKeys = getAllActiveKeys(sockets)
+    shrinkedSegments = getShrinkedSegments(activeKeys,segmentkeys_after)
+
     for segment in segmentPids:
         pid = segmentPids[segment] 
         #print "Sending SIGHUP to %i"%(pid)
