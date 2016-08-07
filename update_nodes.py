bf53e858bf331e229f027f0db0bc3a5767c22832
Leonard <leonard@penzer.de>
2016-08-07 11:18:09 +0200

    various changes

--- update_nodes.py
+++ update_nodes.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 import ujson as json
 import time
-
+import sys
+import traceback
 from georeference import getRegion
 
 import email
 import smtplib
 from email.mime.text import MIMEText
 import batman_adv
+import argparse
 
 def send(sender,to,subject, message):
 
@@ -28,7 +30,6 @@ def send(sender,to,subject, message):
     s.sendmail(sender, [to], msg.as_string())
     s.quit()
 
-import argparse
 
 parser = argparse.ArgumentParser(description='Update nodes json file')
 
@@ -43,6 +44,10 @@ parser.add_argument('--alfred-158', dest='alfred158', action='store',
 parser.add_argument('--alfred-159', dest='alfred159', action='store',
             required=True,
            help='Alfred 159 channel data')
+
+parser.add_argument('--alfred-160', dest='alfred160', action='store',
+            required=True,
+           help='Alfred 160 channel data')
 
 parser.add_argument('--nodes', dest='nodes', action='store',
             required=True,
@@ -62,7 +67,6 @@ parser.add_argument('--mailfrom', dest='mailfrom', action='store',
 
 
 args = parser.parse_args()
-#originators = batman_adv.o
 transtable = batman_adv.getTranstableGlobal()
 
 def getFastdConnections(node,gk):
@@ -89,24 +93,43 @@ def getSegment(gateway):
         print "Exception with GW %s"%gateway
         return None
 
+def getNodeConnections():
+    node_connections = {}
+    for n in nodes_mesh:
+        neighbours = []
+        if "batadv" in nodes_mesh[n]:
+            for iface in nodes_mesh[n]["batadv"]:
+                neighbours+= nodes_mesh[n]["batadv"][iface]["neighbours"].keys()
+        #print  nm[n]["batadv"]
+        gws = []
+        for g in neighbours:
+            if g.startswith("02:00:3") or g.startswith("02:00:0a:3"):
+                gws.append(g)
+
+        nodes = [x for x in neighbours if x not in gws]
+        #gws = [x for x in neighbours if x in gk]
+        node_connections[n] ={"nodes": nodes, "gws":gws}
+    return node_connections
+
 fp158 = open(args.alfred158,"rb")
 fp159 = open(args.alfred159,"rb")
+fp160 = open(args.alfred160,"rb")
 fp_nodes_all = open(args.nodes,"rb")
-nodes_online_info = json.load(fp158)
-nodes_online_status = json.load(fp159)
+nodes_info = json.load(fp158)
+nodes_status = json.load(fp159)
+nodes_mesh = json.load(fp160)
 nodes_all = json.load(fp_nodes_all)
 fp_nodes_all.close()
 
-gk = []
-for node in nodes_all:
-    n = nodes_all[node]
-    if n["gateway"] != "" and not n["gateway"] in gk:
-        gk.append(n["gateway"])
+#build up gateway list
+gk = batman_adv.getGateways()
+gk.append("02:00:38:03:07:02")
 gk.sort()
 
+node_connections = getNodeConnections()
 
-for mac in nodes_online_info:
-    n = nodes_online_info[mac]
+for mac in nodes_info:
+    n = nodes_info[mac]
     if not n.has_key("owner"):
         n["owner"] = ""
     if not nodes_all.has_key(mac):
@@ -114,12 +137,21 @@ for mac in nodes_online_info:
         nodes_all[mac] = {}        
         name = None
         try:
-            name = nodes_online_info[mac]["hostname"]
-            mail.send(args.mailfrom,args.mailto,"New Node anounced","MAC: %s\nName: %s\n"%(mac,name))
+            name = "unknown"
+            if mac in nodes_info and "hostname" in nodes_info[mac]:
+                name = nodes_info[mac]["hostname"]
+            no = nodes_info[mac]
+            region = "Unknown"
+            if no.has_key("location"):
+                if "longitude" in no["location"] and "latitude" in no["location"]:
+                    (region,desiredSegment) = getRegion(no["location"]["latitude"],no["location"]["longitude"])
+            send(args.mailfrom,args.mailto,"New Node anounced","MAC: %s\nName: %s\nregion %s\nSegment: %s"%(mac,name,region,desiredSegment))
         except:
             print "Error sending mail for mac %s"%(mac)
             print name
+            traceback.print_exc(file=sys.stdout)
             pass
+
 hiddenNodes = []
 alfred = 0
 tt = 0
@@ -128,15 +160,17 @@ for mac in nodes_all:
     n = nodes_all[mac]
     if n.has_key("owner"):
         n.pop("owner")
-    if nodes_online_info.has_key(mac) and nodes_online_status.has_key(mac):
+    if nodes_info.has_key(mac) and nodes_status.has_key(mac):
         alfred+=1   
-        no = nodes_online_info[mac]
-        nos = nodes_online_status[mac]
+        no = nodes_info[mac]
+        nos = nodes_status[mac]
         n["status"] = "online"
         n["hostname"] = no["hostname"]
         n["hardware"] = no["hardware"]
         n["network"] = no["network"]
         n["loadavg"] = nos["loadavg"]
+        if mac in node_connections:
+            n["neighbours"] = node_connections[mac]["nodes"]
         if nos.has_key("gateway"):
             n["gateway"] = nos["gateway"]
             n["segment"] = getSegment(nos["gateway"])
@@ -153,8 +187,8 @@ for mac in nodes_all:
         n["software"] = no["software"]
         n["last_online"] = int(time.time())
         n["last_alfred"] = int(time.time())
-        if nodes_online_status[mac].has_key("clients"):
-            n["clients"] = nodes_online_status[mac]["clients"]
+        if nodes_status[mac].has_key("clients"):
+            n["clients"] = nodes_status[mac]["clients"]
         n["fastd"] = getFastdConnections(mac,gk)
     elif transtable.has_key(mac):
         tt+=1
@@ -170,6 +204,7 @@ for mac in nodes_all:
         n["clients"]["wifi"] = 0
         n["gateway"] = ""
         n["fastd"] = []
+        n["neighbours"] = []
         deltaWeek = 7*24*60*60
         if n["last_online"] < int(time.time())-deltaWeek:
             hiddenNodes.append(mac)
