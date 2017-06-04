f25b212939278b13d4f560f6c652d0f689dc801c
Leonard Penzer <leonard@penzer.de>
2017-06-04 09:39:27 +0000

    address states now if IPv4, IPv6 or known ds-lite IPv4

--- fastd-status.py
+++ fastd-status.py
@@ -8,7 +8,10 @@ parser = argparse.ArgumentParser(description='Clean fastd output - remove public
 parser.add_argument('-o', dest='output', action='store', required=True, help='Output file')
 parser.add_argument('-i', dest='input', action='store', required=True, help='Input file')
 parser.add_argument('-q',dest='quiet', action='store_true', required=False, help='Do not warn if input file does not exist')
+parser.add_argument('--show',dest='show', action='store_true', required=False, help='show ip adresses in output')
 args = parser.parse_args()
+
+dslite = ["46.5.254.26",]
 
 if not os.path.exists(args.input):
     if not args.quiet: 
@@ -28,7 +31,14 @@ s.close()
 status = json.loads(result)
 
 for p in  status["peers"]:
-        if status["peers"][p]["address"] != "any":
+        if status["peers"][p]["address"] != "any" and not args.show:
+            if status["peers"][p]["address"].split(":")[0] in dslite:
+                status["peers"][p]["address"] = "ds-lite"
+            elif "." in status["peers"][p]["address"]:
+                status["peers"][p]["address"] = "IPv4"
+            elif ":" in status["peers"][p]["address"]:
+                status["peers"][p]["address"] = "IPv6"
+            else:
                 status["peers"][p]["address"] = "available"
 
 try:
