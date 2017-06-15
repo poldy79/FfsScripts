c91c511299bd2677dd7ed39c22e2688824767720
Leonard Penzer <leonard@penzer.de>
2017-06-15 18:55:42 +0000

    added ffsl3 pull and fastd bb status output

--- housekeeping.sh
+++ housekeeping.sh
@@ -2,9 +2,11 @@
 cd ~/freifunk
 for seg in {00..63}; do
 ./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpn$seg.sock -o /var/www/html/data/vpn$seg.json
+./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpn${seg}bb.sock -o /var/www/html/data/vpn${seg}bb.json
 done
 
 ~/freifunk/FfsScripts/update_peers.sh /etc/fastd/peers-ffs > /dev/null
 git -C /etc/tinc/ffsbb/tinc-ffsbb pull > /dev/null
+git -C /etc/tinc/ffsl3/tinc pull > /dev/null
 killall -HUP tincd
 
