713ddbc964978f40a4495ca37d9d65963de03255
Leonard Penzer <leonard@penzer.de>
2020-03-09 22:17:03 +0000

    Aktueller Stand vor alfred phase-out

--- carbon.py
+++ carbon.py
@@ -32,32 +32,31 @@ def commitData(timestamp):
         if node not in nodes_158:
             continue
         hostname = nodes_158[node]["hostname"]
-        if not hostname.startswith("ffs-nbhfstr163"):
-            continue
-        n = nodes[node]
-        host =  "alfred."+hostname+"."
-        data.append((host+"clients",n["clients"]["total"]))
-        data.append((host+"loadavg",n["loadavg"]))
-        data.append((host+"memory.buffers",n["memory"]["buffers"]))
-        data.append((host+"memory.cached",n["memory"]["cached"]))
-        data.append((host+"memory.free",n["memory"]["free"]))
-        data.append((host+"memory.total",n["memory"]["total"]))
-        data.append((host+"processes.running",n["processes"]["running"]))
-        data.append((host+"processes.total",n["processes"]["total"]))
-        data.append((host+"traffic.forward.bytes",n["traffic"]["forward"]["bytes"]))
-        data.append((host+"traffic.forward.packets",n["traffic"]["forward"]["packets"]))
-        data.append((host+"traffic.mgmt_rx.bytes",n["traffic"]["mgmt_rx"]["bytes"]))
-        data.append((host+"traffic.mgmt_rx.packets",n["traffic"]["mgmt_rx"]["packets"]))
-        data.append((host+"traffic.mgmt_tx.bytes",n["traffic"]["mgmt_tx"]["bytes"]))
-        data.append((host+"traffic.mgmt_tx.packets",n["traffic"]["mgmt_tx"]["packets"]))
-        data.append((host+"traffic.rx.bytes",n["traffic"]["rx"]["bytes"]))
-        data.append((host+"traffic.rx.packets",n["traffic"]["rx"]["packets"]))
-        data.append((host+"traffic.tx.bytes",n["traffic"]["tx"]["bytes"]))
-        data.append((host+"traffic.tx.packets",n["traffic"]["tx"]["packets"]))
-        data.append((host+"uptime",n["uptime"]))
-        print data
-        submit(data,timestamp)
-        print("wrote data for %s"%(hostname))
+        if hostname.startswith("ffs-nbhfstr163") or hostname.startswith("ffs-es-altstadt") or hostname.startswith("71522-") or hostname.startswith("71546-"):
+            n = nodes[node]
+            host =  "alfred."+hostname+"."
+            data.append((host+"clients",n["clients"]["total"]))
+            data.append((host+"loadavg",n["loadavg"]))
+            data.append((host+"memory.buffers",n["memory"]["buffers"]))
+            data.append((host+"memory.cached",n["memory"]["cached"]))
+            data.append((host+"memory.free",n["memory"]["free"]))
+            data.append((host+"memory.total",n["memory"]["total"]))
+            data.append((host+"processes.running",n["processes"]["running"]))
+            data.append((host+"processes.total",n["processes"]["total"]))
+            data.append((host+"traffic.forward.bytes",n["traffic"]["forward"]["bytes"]))
+            data.append((host+"traffic.forward.packets",n["traffic"]["forward"]["packets"]))
+            data.append((host+"traffic.mgmt_rx.bytes",n["traffic"]["mgmt_rx"]["bytes"]))
+            data.append((host+"traffic.mgmt_rx.packets",n["traffic"]["mgmt_rx"]["packets"]))
+            data.append((host+"traffic.mgmt_tx.bytes",n["traffic"]["mgmt_tx"]["bytes"]))
+            data.append((host+"traffic.mgmt_tx.packets",n["traffic"]["mgmt_tx"]["packets"]))
+            data.append((host+"traffic.rx.bytes",n["traffic"]["rx"]["bytes"]))
+            data.append((host+"traffic.rx.packets",n["traffic"]["rx"]["packets"]))
+            data.append((host+"traffic.tx.bytes",n["traffic"]["tx"]["bytes"]))
+            data.append((host+"traffic.tx.packets",n["traffic"]["tx"]["packets"]))
+            data.append((host+"uptime",n["uptime"]))
+            #print(data)
+            submit(data,timestamp)
+            #print("wrote data for %s"%(hostname))
 
 def main():
     timestamp = int(time.time())
