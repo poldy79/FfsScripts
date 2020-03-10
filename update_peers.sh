9b3896212dbd6fab13df7c35ed5b2da1d17fd032
Leonard Penzer <leonard@penzer.de>
2020-03-10 19:51:43 +0000

    Führe vor pull ein fetch aus.

--- update_peers.sh
+++ update_peers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git -C $1 pull > /dev/null
+git -C $1 fetch -q > /dev/null
+git -C $1 pull -q > /dev/null
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/update_peers.py --repo $1
