614e12c1ff65a237b08c20f4f5c3d358d5bac44d
Leonard Penzer <leonard@penzer.de>
2020-03-10 19:54:20 +0000

    fastd-status für alle instanzen, git fetch vor pull

--- housekeeping.sh
+++ housekeeping.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 cd ~/freifunk
-for seg in {00..63}; do
+./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpn00.sock -o /var/www/html/data/vpn00.json
+for seg in {01..32}; do
 ./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpn$seg.sock -o /var/www/html/data/vpn$seg.json
-./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpn${seg}bb.sock -o /var/www/html/data/vpn${seg}bb.json
+./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpy${seg}.sock -o /var/www/html/data/vpy${seg}.json
+./FfsScripts/fastd-status.py -q -i /var/run/fastd-bb${seg}.sock -o /var/www/html/data/bb${seg}.json
 done
 
 ~/freifunk/FfsScripts/update_peers.sh /etc/fastd/peers-ffs > /dev/null
-git -C /etc/tinc/ffsbb/tinc-ffsbb pull > /dev/null
-git -C /etc/tinc/ffsl3/tinc pull > /dev/null
+git -C /etc/tinc/ffsbb/tinc-ffsbb fetch -q > /dev/null
+git -C /etc/tinc/ffsbb/tinc-ffsbb pull -q > /dev/null
+git -C /etc/tinc/ffsl3/tinc fetch -q > /dev/null
+git -C /etc/tinc/ffsl3/tinc pull -q > /dev/null
 killall -HUP tincd
-
+~/freifunk/FfsScripts/genGwStatus.py -o /var/www/html/data/gwstatus.json
