cca520ca3202391e040c2ea78f5c875239245754
Leonard Penzer <leonard@penzer.de>
2020-03-10 19:38:06 +0000

    Verwende raw.json anstelle von nodes.json datenbank

--- update_appmap.py
+++ update_appmap.py
@@ -11,7 +11,7 @@ parser.add_argument('-o', dest='output', action='store',
                         required=True,
                    help='Output file')
 
-parser.add_argument('--nodes', dest='nodes', action='store',
+parser.add_argument('--raw', dest='raw', action='store',
                                         required=True,
                                                                                            help='nodes file')
 
@@ -22,9 +22,9 @@ args = parser.parse_args()
 
 ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%S')
-fp = open(args.nodes,"rb")
+fp = open(args.raw,"rb")
 
-nodes = json.load(fp)
+raw = json.load(fp)
 fp.close()
 
 
@@ -35,16 +35,15 @@ m["meta"] = { "timestamp": st }
 m["links"] = []
 m["nodes"] = []
 
+nodes = raw["nodes"]
 
 for node in nodes:
-	n = nodes[node]
-	if n["status"] == "offline":
-		status = False
-	else:
-		status = True
-	current =  {"id":node,"name":n["hostname"],"flags": { "online": status, "gateway":False} ,"clientcount":n["clients"]["total"],"firmware": n["software"]["firmware"]["base"] }
-	if n.has_key("location") and "latitude" in n["location"] and "longitude" in n["location"]:
-		pos = [ n["location"]["latitude"], n["location"]["longitude"] ]
+	n = node["statistics"]
+        nodeinfo = node["nodeinfo"]
+        status = node["online"]
+	current =  {"id":node["nodeinfo"]["network"]["mac"],"name":node["nodeinfo"]["hostname"],"flags": { "online": status, "gateway":False} ,"clientcount":n["clients"]["total"],"firmware": node["nodeinfo"]["software"]["firmware"]["base"] }
+	if nodeinfo.has_key("location") and "latitude" in nodeinfo["location"] and "longitude" in nodeinfo["location"]:
+		pos = [ nodeinfo["location"]["latitude"], nodeinfo["location"]["longitude"] ]
 		current["geo"] = pos
 	else:
 		current["geo"] = None
