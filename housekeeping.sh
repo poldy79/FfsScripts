ba13bfc9f322dd85297fc0fad3b46c92f72e8d6e
Leonard Penzer <leonard@penzer.de>
2022-02-27 17:12:38 +0100

    Remove tinc support

--- housekeeping.sh
+++ housekeeping.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd ~/freifunk
-./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpn00.sock -o /var/www/html/data/vpn00.json
 for seg in {01..32}; do
 ./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpn$seg.sock -o /var/www/html/data/vpn$seg.json
-./FfsScripts/fastd-status.py -q -i /var/run/fastd-vpy${seg}.sock -o /var/www/html/data/vpy${seg}.json
 ./FfsScripts/fastd-status.py -q -i /var/run/fastd-bb${seg}.sock -o /var/www/html/data/bb${seg}.json
 done
 
@@ -12,5 +10,8 @@ git -C /etc/tinc/ffsbb/tinc-ffsbb fetch -q > /dev/null
 git -C /etc/tinc/ffsbb/tinc-ffsbb pull -q > /dev/null
 git -C /etc/tinc/ffsl3/tinc fetch -q > /dev/null
 git -C /etc/tinc/ffsl3/tinc pull -q > /dev/null
-killall -HUP tincd
-~/freifunk/FfsScripts/genGwStatus.py -o /var/www/html/data/gwstatus.json
+#killall -HUP tincd
+
+# replaced by extra cron job !!  ~/freifunk/FfsScripts/genGwStatus.py -o /var/www/html/data/gwstatus.json
+# -> new script is "/var/lib/ffs/loadbalancer/genGwStatus.py"
+
