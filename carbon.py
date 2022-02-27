1c90aec07f245e40c3068e0d60300335617cbd5a
Leonard Penzer <leonard@penzer.de>
2022-02-27 16:01:47 +0100

    Add collectd output support

--- carbon.py
+++ carbon.py
@@ -9,13 +9,16 @@ import argparse
 
 CARBON_SERVER = '10.191.255.243'
 CARBON_PICKLE_PORT = 2004
-
+INTERVAL = 60
 
 class Carbon():
-    def __init__(self, timestamp, input, carbon):
+    def __init__(self, timestamp, input, carbon, collectd, dump, verbose):
         self.timestamp = timestamp
         self.carbon = carbon
         self.input = input
+        self.dump = dump
+        self.verbose = verbose
+        self.collectd = collectd
 
     def mac2met(self, mac):
         s = mac.split(":")
@@ -23,6 +26,13 @@ class Carbon():
             return "gw%sn01.s%s" % (s[5], s[4])
         else:
             return "gw%sn%s.s%s" % (s[4], s[5], s[3])
+
+    def convert_mac(self, mac):
+        s = mac.split(":")
+        if s[2] == "0a":
+            return f"gw{s[5]}n01", s[4]
+        else:
+            return f"gw{s[4]}n{s[5]}", s[3]
 
     def submit_to_carbon(self, data):
         sock = socket.socket()
@@ -74,14 +84,21 @@ class Carbon():
         for mac in gw:
             # print(mac2met(mac))
             g = gw[mac]
-            data = []
+            carbon_data = []
+            prometheus_data = []
+            gw_name, segment = self.convert_mac(mac)
             prefix = "alfred_gw.%s." % (self.mac2met(mac))
-            data.append((prefix + "clients", g["clients"]))
-            data.append((prefix + "nodes", g["nodes"]))
-            data.append((prefix + "fastd", g["fastd"]))
-            # print(data)
+            carbon_data.append((prefix + "clients", g["clients"]))
+            carbon_data.append((prefix + "nodes", g["nodes"]))
+            carbon_data.append((prefix + "fastd", g["fastd"]))
+            prometheus_data.append(f'PUTVAL "{gw_name}/respondd_segment-{segment}/gauge-clients" N:{g["clients"]}')
+
+            # print(carbon_data)
             if self.carbon:
-                self.submit_to_carbon(data)
+                self.submit_to_carbon(carbon_data)
+            if self.collectd:
+                # print(carbon_data)
+                print("\n".join(prometheus_data))
         if len(errors) > 0:
             print("Eroors with nodes: %s" % (" ".join(errors)))
 
@@ -119,6 +136,8 @@ class Carbon():
                     data.append((host + "uptime", n["uptime"]))
                     if self.carbon:
                         self.submit_to_carbon(data)
+                    if self.dump:
+                        print(data)
                     # print("wrote data for %s"%(hostname))
             except KeyError as e:
                 pass
@@ -135,20 +154,29 @@ class Carbon():
             raise
 
     def run(self):
-        self.load_json()
-        self.commitDataNodeStats()
-        self.commitDataGwStats()
-
+        while(True):
+            self.load_json()
+            self.commitDataNodeStats()
+            self.commitDataGwStats()
+            if not self.collectd:
+                break
+            time.sleep(INTERVAL)
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--carbon', action='store_true',
                         help='push data to carbon')
+    parser.add_argument('--collectd', action='store_true',
+                        help='print collectd text format')
+    parser.add_argument('--verbose', action='store_true',
+                        help='print verbose')
+    parser.add_argument('--dump', action='store_true',
+                        help='dump data')
     parser.add_argument('-i', '--input', required=True,
                         help='input file raw.json')
     args = parser.parse_args()
     timestamp = int(time.time())
-    carbon = Carbon(timestamp, input=args.input, carbon=args.carbon)
+    carbon = Carbon(timestamp, input=args.input, carbon=args.carbon, collectd=args.collectd, dump=args.dump, verbose=args.verbose)
     carbon.run()
 
 
